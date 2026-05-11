6a2b75d8559d62f48370db05ff35dd4ec8be072a
黄旭涛 23301098 <23301098@bjtu.edu.cn>
2026-05-11 11:26:36 +0800

    23301098 实验2-第一次提交 准备bin和main.rs，编译生成内核镜像

---
+++ lab2/main.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![no_main]
+#[macro_use]
+
+mod console;
+mod lang_items;
+mod sbi;
+
+use core::arch::global_asm;
+
+global_asm!(include_str!("entry.asm"));
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    let sbss_ptr = sbss as *const () as usize;
+    let ebss_ptr = ebss as *const () as usize;
+    (sbss_ptr..ebss_ptr).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
+    clear_bss();
+    println!("Hello, world!");
+    println!(".text [{:#x}, {:#x})", stext as *const () as usize, etext as *const () as usize);
+    println!(".rodata [{:#x}, {:#x})", srodata as *const () as usize, erodata as *const () as usize);
+    println!(".data [{:#x}, {:#x})", sdata as *const () as usize, edata as *const () as usize);
+    println!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as *const () as usize, boot_stack_top as *const () as usize
+    );
+    println!(".bss [{:#x}, {:#x})", sbss as *const () as usize, ebss as *const () as usize);
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
