40481a4bc3f187b6ee07841454379c92b94a9abd
黄旭涛 23301098 <23301098@bjtu.edu.cn>
2026-05-23 15:12:35 +0800

    23301098 黄旭涛 实验3-第三次提交 更新main.rs并成功运行批处理操作系统

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,10 +5,14 @@
 mod console;
 mod lang_items;
 mod sbi;
+mod syscall;
+mod trap;
+mod batch;
 
 use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
 fn clear_bss() {
     extern "C" {
         fn sbss();
@@ -20,7 +24,7 @@ fn clear_bss() {
 }
 
 #[no_mangle]
-pub fn rust_main() -> ! {
+/*pub fn rust_main() -> ! {
     extern "C" {
         fn stext();
         fn etext();
@@ -45,4 +49,11 @@ pub fn rust_main() -> ! {
     println!(".bss [{:#x}, {:#x})", sbss as *const () as usize, ebss as *const () as usize);
     println!("Hello, world!");
     panic!("Shutdown machine!");
+}*/
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("[Kernel] Hello, world!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
