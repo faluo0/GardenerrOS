c9f78eb378646523ae70e14478eb283d863cc7b9
黄旭涛 23301098 <23301098@bjtu.edu.cn>
2026-06-07 20:23:46 +0800

    23301098 黄旭涛 实验5-第一次提交 实现分时多任务与抢占式调度，make run通过

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -2,6 +2,7 @@ use crate::task::{
     suspend_current_and_run_next,
     exit_current_and_run_next,
 };
+use crate::timer::get_time_ms;
 
 pub fn sys_exit(exit_code: i32) -> ! {
     println!("[kernel] Application exited with code {}", exit_code);
@@ -14,3 +15,6 @@ pub fn sys_yield() -> isize {
     0
 }
 
+pub fn sys_get_time() -> isize {
+    get_time_ms() as isize
+}
--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -17,12 +17,22 @@ use riscv::register::{
         self,
         Trap,
         Exception,
+        Interrupt,
     },
     stval,
+    sie,
 };
 
 use crate::syscall::syscall;
-//use crate::batch::run_next_app;
+use crate::task::{
+    exit_current_and_run_next,
+    suspend_current_and_run_next,
+};
+use crate::timer::set_next_trigger;
+
+pub fn enable_timer_interrupt() {
+    unsafe { sie::set_stimer(); }
+}
 
 #[no_mangle]
 pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
@@ -35,12 +45,16 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
         }
         Trap::Exception(Exception::StoreFault) |
         Trap::Exception(Exception::StorePageFault) => {
-            println!("[kernel] PageFault in application, core dumped.");
-            crate::task::exit_current_and_run_next();
+            println!("[kernel] PageFault in application, bad addr = {:#x}, bad instruction = {:#x}, core dumped.", stval, cx.sepc);
+            exit_current_and_run_next();
         }
         Trap::Exception(Exception::IllegalInstruction) => {
             println!("[kernel] IllegalInstruction in application, core dumped.");
-            crate::task::exit_current_and_run_next();
+            exit_current_and_run_next();
+        }
+        Trap::Interrupt(Interrupt::SupervisorTimer) => {
+            set_next_trigger();
+            suspend_current_and_run_next();
         }
         _ => {
             panic!("Unsupported trap {:?}, stval = {:#x}!", scause.cause(), stval);
@@ -50,4 +64,3 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
 }
 
 pub use context::TrapContext;
-
